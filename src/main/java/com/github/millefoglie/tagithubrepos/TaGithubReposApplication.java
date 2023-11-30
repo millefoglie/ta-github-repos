4114de3d5cdf3b971022874fbd64fe90bcd0aaed
millefoglie <millefoglie@users.noreply.github.com>
2023-11-30 17:19:42 +0200

    Initial commit

---
+++ src/main/java/com/github/millefoglie/tagithubrepos/TaGithubReposApplication.java
@@ -0,0 +1,13 @@
+package com.github.millefoglie.tagithubrepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaGithubReposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaGithubReposApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/millefoglie/tagithubrepos/TaGithubReposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.millefoglie.tagithubrepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaGithubReposApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
